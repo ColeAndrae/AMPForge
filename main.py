e8742af0637b9fa2262d63d7be2cc21cc385d496
Cole Andrae <cole@theandraes.com>
2025-08-15 08:49:45 -0400

    bug fixes

--- main.py
+++ main.py
@@ -275,7 +275,7 @@ def main():
         if st.session_state.pdb_structure:
             st.markdown("#### Interactive 3D Visualization")
             view = create_3d_visualization(st.session_state.pdb_structure)
-            col1, col2, col3 = st.columns([1, 1, 1])
+            col1, col2, col3 = st.columns([1, 2, 1])
             with col2:
                 showmol(view, height=600, width=800)
 
